ab436c440d809c1c8a127e0d1e35135229f51729
Federico Ponzi <isaacisback92@gmail.com>
2020-04-21 14:55:54 +0200

    cleanup healthchecker, simplifies logic and use traits

---
+++ src/horust/healthcheck/checks.rs
@@ -0,0 +1,35 @@
+pub(crate) trait Check {
+    fn run(&self, healthiness: &Healthiness) -> bool;
+}
+
+pub(crate) struct HttpCheck;
+impl Check for HttpCheck {
+    fn run(&self, healthiness: &Healthiness) -> bool {
+        healthiness
+            .http_endpoint.as_ref()
+            .map(|endpoint| {
+                if cfg!(not(feature = "http-healthcheck")){
+                    error!("There is an http based healthcheck, but horust was built without the http-healthcheck feature (thus it will never pass these checks).");
+                    return false;
+                }
+                #[cfg(feature = "http-healthcheck")]
+                    {
+                        let client = Client::new();
+                        let resp: reqwest::blocking::Response = client.head(endpoint).send().unwrap();
+                        resp.status().is_success()
+                    }
+            })
+            .unwrap_or(true)
+    }
+}
+
+pub(crate) struct FilePathCheck;
+impl Check for FilePathCheck {
+    fn run(&self, healthiness: &Healthiness) -> bool {
+        healthiness
+            .file_path
+            .as_ref()
+            .map(|file_path| file_path.exists())
+            .unwrap_or(true)
+    }
+}
--- src/horust/healthcheck/mod.rs
+++ src/horust/healthcheck/mod.rs
@@ -5,6 +5,9 @@ use reqwest::blocking::Client;
 use std::collections::HashMap;
 use std::time::Duration;
 
+mod checks;
+use checks::*;
+
 // TODO:
 // * Tunable healthchecks timing in horust's config
 // * If there are no checks to run, just exit the thread. or go sleep until an "service created" event is received.
@@ -17,8 +20,10 @@ pub fn spawn(bus: BusConnector<Event>, services: Vec<Service>) {
 #[derive(Debug)]
 struct Repo {
     bus: BusConnector<Event>,
+
     services: HashMap<ServiceName, Service>,
     /// Keep track of services which have a pid and need check for progressing to the running state
+    //TODO: just an hashset of servicenames
     started: HashMap<ServiceName, Service>,
     /// Keep track of running services
     running: HashMap<ServiceName, Service>,
@@ -81,42 +86,28 @@ impl Repo {
 }
 
 #[cfg(feature = "http-healthcheck")]
+#[cfg(not(test))]
 fn check_http_endpoint(endpoint: &str) -> bool {
     let client = Client::new();
     let resp: reqwest::blocking::Response = client.head(endpoint).send().unwrap();
     resp.status().is_success()
 }
 
+#[cfg(test)]
+fn check_http_endpoint(endpoint: &str) -> bool {
+    return true;
+}
+
 /// Returns true if the service is healthy and all checks are passed.
-fn healthchecker(healthiness: &Healthiness) -> bool {
-    // Count of required checks:
-    let mut checks = 0;
-    // Count of passed checks:
-    let mut checks_res = 0;
-    if let Some(file_path) = healthiness.file_path.as_ref() {
-        checks += 1;
-        checks_res += if file_path.exists() {
-            1
-        } else {
-            debug!("Healthcheck: File: {:?}, doesn't exists yet.", file_path);
-            0
-        };
-    }
-    if let Some(endpoint) = healthiness.http_endpoint.as_ref() {
-        let check_feature = |endpoint: &String| {
-            #[cfg(not(feature = "http-healthcheck"))]
-            {
-                error!("There is an http based healthcheck for {}, requesting: {} , but horust was built without the http-healthcheck feature (thus it will never pass these checks).", sh.name(), endpoint);
-                return (1, 0);
-            }
-            #[cfg(feature = "http-healthcheck")]
-            return (1, if check_http_endpoint(endpoint) { 1 } else { 0 });
-        };
-        let (check, res) = check_feature(endpoint);
-        checks += check;
-        checks_res += res
-    }
-    checks == checks_res
+fn check_health(healthiness: &Healthiness) -> bool {
+    let file = FilePathCheck {};
+    let http = HttpCheck {};
+    let checks: Vec<&dyn Check> = vec![&file, &http];
+    checks
+        .into_iter()
+        .filter(|check| !check.run(healthiness))
+        .count()
+        == 0
 }
 
 /// Run the healthchecks, produce the event changes
@@ -131,13 +122,13 @@ fn next(
         .iter()
         .filter(|(s_name, service)| {
             debug!("going to check {}, which is starting...", s_name);
-            healthchecker(&service.healthiness)
+            check_health(&service.healthiness)
         })
         .map(|(s_name, _service)| Event::new_status_changed(s_name, ServiceStatus::Running));
 
     running
         .iter()
-        .filter(|(_s_name, service)| !healthchecker(&service.healthiness))
+        .filter(|(_s_name, service)| !check_health(&service.healthiness))
         .map(|(service_name, _service)| {
             // TODO: change to ToBeKilled. If the healthcheck fails, maybe it's a transient failure and process might be still running.
             Event::Kill(service_name.into())
@@ -157,7 +148,7 @@ fn run(bus: BusConnector<Event>, services: Vec<Service>) {
             debug!("Breaking the loop..");
             break;
         }
-        std::thread::sleep(Duration::from_millis(500));
+        std::thread::sleep(Duration::from_millis(1000));
     }
 
     repo.send_ev(Event::new_exit_success("Healthcheck"));
@@ -177,7 +168,7 @@ mod test {
     use crate::horust::error::Result;
     use crate::horust::formats::{Event, Healthiness, Service, ServiceName, ServiceStatus};
     use crate::horust::healthcheck;
-    use crate::horust::healthcheck::healthchecker;
+    use crate::horust::healthcheck::check_health;
     use std::collections::HashMap;
     use tempdir::TempDir;
 
@@ -197,7 +188,7 @@ file-path = "{}""#,
             .into_iter()
             .collect();
         let events: Vec<Event> = healthcheck::next(&HashMap::new(), &starting);
-        println!("{:?}", events);
+        debug!("{:?}", events);
         assert!(events.contains(&Event::StatusChanged(
             service.name.clone(),
             ServiceStatus::Running
@@ -205,7 +196,7 @@ file-path = "{}""#,
         Ok(())
     }
     #[test]
-    fn test_healthiness_checks() -> Result<()> {
+    fn test_healthiness_check_file() -> Result<()> {
         // _no_checks_needed
         let tempdir = TempDir::new("health")?;
         let file_path = tempdir.path().join("file.txt");
@@ -213,11 +204,11 @@ file-path = "{}""#,
             file_path: Some(file_path.clone()),
             http_endpoint: None,
         };
-        assert!(!healthchecker(&healthiness));
+        assert!(!check_health(&healthiness));
         std::fs::write(file_path, "Hello world!")?;
-        assert!(healthchecker(&healthiness));
+        assert!(check_health(&healthiness));
         let healthiness: Healthiness = Default::default();
-        assert!(healthchecker(&healthiness));
+        assert!(check_health(&healthiness));
         Ok(())
     }
 }
