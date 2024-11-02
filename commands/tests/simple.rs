00137440a122ad8670fa77d30a288341fb118c12
Federico Ponzi <me@fponzi.me>
2024-11-02 13:10:05 +0000

    Implement commands crate to handle status command

--- commands/tests/simple.rs
+++ commands/tests/simple.rs
@@ -1,31 +1,76 @@
 use anyhow::Result;
-use horust_commands_lib::{CommandsUdsConnectionHandler, CommandsUdsServer};
-use std::path::PathBuf;
-use tracing::info;
-use tracing_test::traced_test;
+use std::os::unix::net::UnixListener;
 
-#[tokio::test]
-#[traced_test]
-async fn test_simple() -> Result<()> {
+use horust_commands_lib::{ClientHandler, CommandsHandlerTrait, HorustMsgServiceStatus};
+use log::info;
+use std::path::PathBuf;
+use std::sync::{Arc, Barrier};
+use std::thread;
+
+struct MockCommandsHandler {
+    unix_listener: UnixListener,
+}
+impl MockCommandsHandler {
+    // full socket path (not the folder).
+    pub fn new(socket_path: PathBuf) -> Self {
+        Self {
+            unix_listener: UnixListener::bind(socket_path).unwrap(),
+        }
+    }
+}
+impl CommandsHandlerTrait for MockCommandsHandler {
+    fn get_unix_listener(&mut self) -> &mut UnixListener {
+        &mut self.unix_listener
+    }
+
+    fn get_service_status(&self, service_name: &str) -> Result<HorustMsgServiceStatus> {
+        Ok(match service_name {
+            "Running" => HorustMsgServiceStatus::Running,
+            "Started" => HorustMsgServiceStatus::Started,
+            _ => unimplemented!(),
+        })
+    }
+
+    fn update_service_status(
+        &self,
+        service_name: &str,
+        new_status: HorustMsgServiceStatus,
+    ) -> Result<()> {
+        todo!()
+    }
+}
+fn init() {
+    let _ = env_logger::builder().is_test(true).try_init();
+}
+#[test]
+fn test_simple() -> Result<()> {
     info!("Starting");
+    init();
 
     let socket_path: PathBuf = "/tmp/simple.sock".into();
     if socket_path.exists() {
         std::fs::remove_file(&socket_path)?;
     }
     let socket_path2 = socket_path.clone();
-    let s_handle = tokio::spawn(async move {
-        let mut uds = CommandsUdsServer::new(&socket_path2).await.unwrap();
+    let barrier_server = Arc::new(Barrier::new(2));
+    let barrier_client = Arc::clone(&barrier_server);
+    let s_handle = thread::spawn(move || {
+        let mut uds = MockCommandsHandler::new(socket_path2);
         info!("uds created");
-        uds.accept().await.unwrap();
+        barrier_server.wait();
+        uds.accept().unwrap();
+        uds.accept().unwrap();
     });
-    let c_handle = tokio::spawn(async {
-        let client = CommandsUdsConnectionHandler::new_client(socket_path)
-            .await
-            .unwrap();
-        client.client().await.unwrap();
+
+    let c_handle = thread::spawn(move || {
+        barrier_client.wait();
+        let client = ClientHandler::new_client(&socket_path).unwrap();
+        client.client("Running".into()).unwrap();
+
+        let client = ClientHandler::new_client(&socket_path).unwrap();
+        client.client("Started".into()).unwrap();
     });
-    s_handle.await?;
-    c_handle.await?;
+    s_handle.join().unwrap();
+    c_handle.join().unwrap();
     Ok(())
 }
