65b8613c5883a98429b584a9da02ee5b708153fd
Chaser Huang <huangkangjing@gmail.com>
2026-03-14 17:16:54 -0400

    fix: Skip unit test in nix

--- default.nix
+++ default.nix
@@ -8,4 +8,7 @@
     nativeBuildInputs = [protobuf];
     src = ./.;
     cargoHash = "sha256-k5h1Z5AsDF+mGqqhGEzRGSxVNGOILKlMjQOoJSlbPUs=";
+    # horust tests need full environment
+    # cannot run in nix build sandboxes
+    doCheck = false;
 }
